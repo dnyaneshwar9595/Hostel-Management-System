7311827718df6a11a09f74f3c32db30b7ccdbffb
dnyaneshwar9595 <dnyaneshwar8029@gmail.com>
2024-03-19 16:26:48 +0530

    connected to database

--- hostelManagementSystem/settings.py
+++ hostelManagementSystem/settings.py
@@ -78,17 +78,11 @@ WSGI_APPLICATION = 'hostelManagementSystem.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.sqlite3',
-#         'NAME': BASE_DIR / 'db.sqlite3',
-#     }
-# }
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql_psycopg2',
         'NAME': 'HostelData',
-        'USER': 'dnyaneshwar',
+        'USER': 'postgres',
         'PASSWORD': 'password',
         'HOST': 'localhost',
         'PORT': '5432',
